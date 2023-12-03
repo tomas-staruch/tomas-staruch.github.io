1c6b8459a6a0970fc469ad02264a5d906f148410
Tomas Staruch <tomas.staruch@gmail.com>
2023-12-03 23:59:14 +0100

    Update menu.service.js

--- AngularJsCourse/module5-solution/src/common/menu.service.js
+++ AngularJsCourse/module5-solution/src/common/menu.service.js
@@ -1,29 +1,44 @@
 (function () {
-"use strict";
+  "use strict";
 
-angular.module('common')
-.service('MenuService', MenuService);
-
-
-MenuService.$inject = ['$http', 'ApiPath'];
-function MenuService($http, ApiPath) {
-  var service = this;
-
-  service.getCategories = function () {
-    return $http.get(ApiPath + '/categories.json').then(function (response) {
-      return response.data;
-    });
-  };
+  angular.module('common')
+  .service('MenuService', MenuService);
 
 
-  service.getMenuItems = function (category) {
-    return $http.get(ApiPath + '/menu_items/' + category + '.json').then(function (response) {
-      return response.data;
-    });
-  };
+  MenuService.$inject = ['$http', 'ApiPath'];
+  function MenuService($http, ApiPath) {
+    var service = this;
+    service.user = null;
 
-}
+    service.saveUser = function(user) {
+      console.log("saveUser:" + JSON.stringify(user));
+
+      service.user = angular.copy(user);
+
+      return true;
+    }
+
+    service.getUser = function() {
+      return service.user;
+    }
+
+    service.getCategories = function () {
+      return $http.get(ApiPath + '/categories.json').then(function (response) {
+        return response.data;
+      });
+    };
 
 
+    service.getMenuItems = function (category) {
+      return $http.get(ApiPath + '/menu_items/' + category + '.json').then(function (response) {
+        return response.data;
+      });
+    };
 
+    service.getFavoriteDish = function(short_name, index) {
+      // e.g. https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/C/menu_items/0.json
+      return $http.get(ApiPath + '/menu_items/' + short_name + '/menu_items/' + index + '.json');
+    }
+
+  }
 })();
